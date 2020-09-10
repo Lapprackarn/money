5c8641129a56c4ae0f770f2aced4dd2499e71434
William Olsson <burrken55@gmail.com>
2020-09-10 10:28:11 +0200

    Add project files.

---
+++ money/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inkomstinfo info = new inkomstinfo();
+            Console.Write("Ange ditt namn: ");
+            info.Namn = Console.ReadLine();
+            Console.Write("Ange din inkomst: ");
+
+
+        
+     
+           
+        }
+    }
+}
---
+++ money/inkomstinfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money
+{
+    class inkomstinfo
+    {
+        public string Namn;
+        public int Inkomst;
+        public int Timmar;
+        public string ShowInkomstInfo()
+        {
+            return Namn + " med inkomst: " + Inkomst;
+        }
+        public string GetNamn()
+        {
+            return namn;
+        }
+        
+    }
+}
